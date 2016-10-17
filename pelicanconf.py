bfa869af8634bfe19c9c5487772d9ce77ab96b79
BabuSubashChandar C <babusubashchandar@zilogic.com>
2016-10-17 18:23:42 +0530

    SITEURL mismatch fixed.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = u'BabuSubashChandar'
 SITENAME = u'BabuSubashChandar'
 SITETITLE = u'Babuenir'
 SITESUBTITLE = u'Random musings on Technology, Art & Science'
-SITEURL = u'http://localhost:8000'
+SITEURL = u'https://babuenir.github.io/blog'
 GRAVATAR_IMAGE = 'https://secure.gravatar.com/avatar/aaf9dadf96ba3361de25e04a6c9fdfc3'
 SITELOGO = SITEURL + u'/static/images/babuenir.png'
 
